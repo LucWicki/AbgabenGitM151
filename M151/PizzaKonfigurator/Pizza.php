0ce80967f3f563cbd709c63c8582ee72212a3514
Luc Wicki <Luc_Wicki@sluz.ch>
2023-02-08 13:12:57 +0100

    Pizza fertig

--- M151/PizzaKonfigurator/Pizza.php
+++ M151/PizzaKonfigurator/Pizza.php
@@ -2,25 +2,38 @@
 // Session Starten
 session_start();
 
-$zutaten = array('');
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $zutat = $_POST['zutat'];
+//1. Daten aus Session lesen
+$ingredients = [];
 
-    if (isset($_SESSION[$zutat])) {
-        $zutaten = $_SESSION[$zutat];
-    }
+//2. Daten aus Session lesen
 
-    foreach ($zutaten as $element) {
-        echo '<br>' . $element;
-    }
-
-//Daten Speichern
-    $_SESSION[$zutat][] = $zutaten;
+if (array_key_exists('ingredients', $_SESSION)) {
+    $ingredients = $_SESSION['ingredients'];
 }
-?>
+//3. Daten verändern
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $newingredients = array_key_exists('ingredient', $_POST) ? $_POST['ingredient'] : '';
+    if($newingredients) {
+        array_push($ingredients, $newingredients);
+        //
+    }
+}
+//Daten Speichern
+    $_SESSION['ingredients'] = $ingredients;
 
+?>
+<h1>Zutat hinzufügen</h1>
 <form method="POST" action="?">
-    <input type="text" name="zutat" placeholder="Zutat" />
-    <input type="submit" value="Absenden" />
+    <input type="text" name="ingredient" placeholder="Zutat eingeben" />
+    <input type="submit" value="Hinzufügen" />
 </form>
+<h2>Deine Zutaten</h2>
+
+<ul>
+    <?php
+        foreach($ingredients as $ingredient){
+    echo "<li>{$ingredient}</li>";
+    }
+    ?>
+</ul>
 
